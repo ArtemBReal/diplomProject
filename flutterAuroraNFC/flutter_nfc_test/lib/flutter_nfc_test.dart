2614997bea242fb46f8572020c6acb2c785e5695
ArtemBReal <artemb.ggw@gmail.com>
2024-03-06 08:08:19 +0300

    Попытки внедрить вызов метода NFCD

--- flutterAuroraNFC/flutter_nfc_test/lib/flutter_nfc_test.dart
+++
@@ -1,8 +0,0 @@
-
-import 'flutter_nfc_test_platform_interface.dart';
-
-class FlutterNfcTest {
-  Future<String?> getPlatformVersion() {
-    return FlutterNfcTestPlatform.instance.getPlatformVersion();
-  }
-}
